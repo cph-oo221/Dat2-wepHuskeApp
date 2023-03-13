7aa1810c8e185fdb8dce3bd67e0db5b9d0196a3f
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-03-13 10:26:20 +0100

    update

--- src/main/java/com/example/dat2wephuskeapp/Controller/AppStart.java
+++ src/main/java/com/example/dat2wephuskeapp/Controller/AppStart.java
@@ -1,16 +1,44 @@
 package com.example.dat2wephuskeapp.Controller;
 
+import com.example.dat2wephuskeapp.Entitet.Person;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.*;
 
 public class AppStart implements ServletContextListener
 {
+    private static Map<String, Person> personMap;
+
     @Override
     public void contextInitialized(ServletContextEvent sce)
     {
       //  ServletContextListener.super.contextInitialized(sce);
-        
+        List<Person> personList = new ArrayList<>();
 
+        Person person = new Person("Oskar", "123", "bamse", "is", "pude" );
+        Person person1 = new Person("Lars", "1234", "tun", "øl", "gaffatape" );
+        Person person2 = new Person("Peter", "12345", "pløkker", "vodka" );
+        Person person3 = new Person("Bob", "123456", "Vand", "gril" );
+
+        personList.add(person);
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+
+        personMap = new TreeMap<>();
+
+        for (Person element : personList)
+        {
+            personMap.put(element.getNavn(), element);
+        }
+
+        // sce.getServletContext().setAttribute("personer", personMap);
+    }
+
+    public static Map<String, Person> getPersonMap()
+    {
+        return personMap;
     }
 
     @Override
---
+++ src/main/java/com/example/dat2wephuskeapp/Entitet/Person.java
@@ -0,0 +1,53 @@
+package com.example.dat2wephuskeapp.Entitet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Person
+{
+    private List<String> stringArrayList;
+    private String navn;
+    private String kode;
+
+    public Person(String navn, String kode, String... s)
+    {
+        this.navn = navn;
+        this.kode = kode;
+
+        this.stringArrayList = Arrays.asList(s);
+//        String [] strings = s.split(",");
+//        this.stringArrayList = new ArrayList<>(Arrays.asList(strings));
+    }
+
+
+    public List<String> getStringArrayList()
+    {
+        return stringArrayList;
+    }
+
+    public void setStringArrayList(List<String> stringArrayList)
+    {
+        this.stringArrayList = stringArrayList;
+    }
+
+    public String getNavn()
+    {
+        return navn;
+    }
+
+    public void setNavn(String navn)
+    {
+        this.navn = navn;
+    }
+
+    public String getKode()
+    {
+        return kode;
+    }
+
+    public void setKode(String kode)
+    {
+        this.kode = kode;
+    }
+}
